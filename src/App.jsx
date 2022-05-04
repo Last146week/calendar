369b567b9ddf8d273bf8750129afeb2314b4d209
Last146week <103073545+Last146week@users.noreply.github.com>
2022-05-04 18:18:56 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Calendar from './components/Calendar';
+
+class App extends React.Component {
+	render() {
+     return (
+    <div>
+     <Calendar />
+    </div>
+  );
+ }
+}
+
+export default App;
